3f4e63e49aae3bd94d1010a43c5efa80ae31812a
Oksberg <oksberg89@gmail.com>
2023-03-02 21:24:01 -0800

    appends string to string using at most n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using
+ * at most n bytes from src
+ * does not need to be null-terminated
+ * if it contains n or more bytes
+ * @src: the string to append
+ * @dest: the string to append to
+ *
+ * Return: pointer to the resulting dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
