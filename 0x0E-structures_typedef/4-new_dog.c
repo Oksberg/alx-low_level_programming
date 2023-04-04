2a4454425603468586ec09d3f4a881093f302a9a
Oksberg <oksberg89@gmail.com>
2023-04-04 08:38:58 -0700

    creates new dog called some_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,45 @@
 #include "dog.h"
-#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @str:  string to be ckecked
+ *
+ * Return: 0;
+ */
+
+int _strlen(char *str)
+{
+	int a = 0; /*start counting here*/
+
+	for (; *str++;)
+		a++;
+	return (a);
+}
+
+/**
+ * *_strcpy - copies string pointed to by src
+ * @dest: copy to
+ * @str: copy from
+ *
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	for (; j < i; j++)
+	{
+		dest[j] = str[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
   * new_dog - creates a new dog
@@ -12,17 +52,37 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-	{
+	int nlen, olen;
+	dog_t *some_dog;
+
+	nlen = _strlen(name);
+	olen = _strlen(owner);
+
+	some_dog = malloc(sizeof(dog_t));
+
+	if (some_dog == NULL)
 		return (NULL);
-		free(new_dog);
+
+	some_dog->owner = malloc(sizeof(char) * (olen + 1));
+	if (some_dog->owner == NULL)
+		return (NULL);
+
+	some_dog->name = malloc(sizeof(char) * (nlen + 1));
+	if (some_dog->owner == NULL)
+		return (NULL);
+
+
+	some_dog->owner = _strcpy(some_dog->owner, owner);
+	some_dog->name = _strcpy(some_dog->name, name);
+	some_dog->age = age;
+
+	if (some_dog->owner == NULL || some_dog->name == NULL)
+	{
+		free(some_dog->owner);
+		free(some_dog->name);
+		free(some_dog);
+		return (NULL);
 	}
 
-	new_dog->owner = strdup(owner);
-	new_dog->name = strdup(name);
-	new_dog->age = age;
-
-	return (new_dog);
-	free(new_dog);
+	return (some_dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
   * struct dog - takes attributes of a dog
   * @name: Name of dog
@@ -25,5 +26,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *str);
 
 #endif
