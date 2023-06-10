a6743bdb975ccf47caa868ff6a2e8a769888f205
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-09 23:14:46 -0700

    Print arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A function that takes arguments from command line
+ * @argc: The number of arguments passed
+ * @argv: An array of arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (count < argc)
+	{
+		printf("%s\n", argv[count]);
+		count++;
+	}
+	return (0);
+}
