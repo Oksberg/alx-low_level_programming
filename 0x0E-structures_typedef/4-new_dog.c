2996e26bce8f3bb1f5e8ef357917f1980b1db298
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-20 06:00:58 -0700

    We've got a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,48 @@
 #include "dog.h"
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
+/**
+  * _strlen - Counts the length of str
+  * @str: String
+  *
+  * Return: Length of string
+  */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+  * _strcpy - Copies contents of src to dest
+  * @dest: Destination string
+  * @src: Source string
+  *
+  * Return: Pointer to destination string
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 /**
   * new_dog - Creates new variable of type dog_t
   * @name: Name of dog
@@ -21,22 +61,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->name = malloc(strlen(name) + 1);
+	ptr->name = malloc(_strlen(name) + 1);
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->name, name);
+	_strcpy(ptr->name, name);
 
-	ptr->owner = malloc(strlen(owner) + 1);
+	ptr->owner = malloc(_strlen(owner) + 1);
 	if (ptr->owner == NULL)
 	{
 		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->owner, owner);
+	_strcpy(ptr->owner, owner);
 
 	ptr->age = age;
 
