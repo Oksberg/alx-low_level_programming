ad72f985c083c271bcc24c13db373e8fee701af0
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-20 05:22:02 -0700

    We've got a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,9 +21,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
+	ptr->name = malloc(strlen(name) + 1);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	strcpy(ptr->name, name);
+
+	ptr->owner = malloc(strlen(owner) + 1);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return (NULL);
+	}
+	strcpy(ptr->owner, owner);
+
 	ptr->age = age;
-	ptr->name = name;
-	ptr->owner = owner;
 
 	return (ptr);
 }
