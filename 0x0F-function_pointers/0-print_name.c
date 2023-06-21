a54364977d2e715ecc6091fb6208b61584a28feb
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-21 09:20:57 -0700

    What's your name?

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
   * print_name - Prints name in two forms
@@ -10,5 +11,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (f != NULL && name != NULL)
+	{
+		(*f)(name);
+	}
 }
