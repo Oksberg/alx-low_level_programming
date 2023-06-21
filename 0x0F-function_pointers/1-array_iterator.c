b461cc3c230854c003e82a5fa123f2a634072e50
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-21 09:48:30 -0700

    Take the necessary action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * array_iterator - Function operates on an array
+  * @array: Array to operate on
+  * @size: Number of elements in the array
+  * @action: Pointer to a function
+  *
+  * Return: Nothing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
