cc1056776c95be38773d6449310996cf1954ee16
Okyere Adubofuor <oksberg89@gmail.com>
2023-07-04 07:23:03 -0700

    What number is in h

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+  * print_listint - Prints all elements of a list
+  * @h: The list to be printed
+  *
+  * Return: Number of elements printed
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	int elements = 0;
+
+	while (h != NULL)
+	{
+		/* Checking if the number element of h exists */
+		if ((*h).n != 0)
+		{
+			printf("%d\n", h->n);
+		}
+		elements++;
+		/* Pointing to the next node */
+		h = h->next;
+	}
+	return (elements);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Task 0
+This function that prints all the elements of a listint_t list. It returns the number of nodes printed and uses the printf function.
