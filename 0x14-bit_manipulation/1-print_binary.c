0e2eec32c6ef8d5ca10e9db4c410eebdb1c53008
Okyere Adubofuor <oksberg89@gmail.com>
2023-07-07 14:49:03 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,21 +14,25 @@ void print_binary(unsigned long int n)
 	int bit, i = 0, j = 0;
 	int realbits = 0, leadingzeros = 1;
 
+	/* Setting condition for when n is 0 */
 	if (n == 0)
 	{
 		printf("0");
 		return;
 	}
 
+	/* Iterating to get the bytes */
 	for (i = sizeof(int); i >= 0; i--)
 	{
 		byte = (n >> (i * 8)) & 0xFF;
 		for (j = 7; j >= 0; j--)
 		{
+			/* Iterating to get the bits */
 			bit = (byte >> j) & 1;
 			if (bit)
 				realbits = 1;
 
+			/* Checking for leading zeros */
 			if (realbits)
 			{
 				leadingzeros = 0;
