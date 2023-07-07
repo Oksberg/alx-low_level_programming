141b2886313442e6b45b56117771300553b94f9e
Okyere Adubofuor <oksberg89@gmail.com>
2023-07-06 18:39:53 -0700

    Converts to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
-
-/**
-  * power - Raises a number to a power
-  * @num: Number to raise to power
-  * @exponent: The power to raise the number to
-  *
-  * Return: Results of the calculation
-  */
-
-unsigned int power(unsigned int num, unsigned int exponent)
-{
-	unsigned int result = 1, i;
-
-	for (i = 0; i < exponent; i++)
-	{
-		result *= num;
-	}
-
-	return (result);
-}
 
 /**
   * binary_to_uint - Converts binary number to decimal
@@ -42,11 +20,18 @@ unsigned int binary_to_uint(const char *b)
 
 	for (count = 0; b[count] != '\0'; count++)
 	{
-		if ((b[count] != '0') && (b[count] != '1'))
+		if ((b[count] == '0') && (b[count] != '1'))
+		{
+			dec_num = dec_num * base;
+		}
+		else if (b[count] == '1')
+		{
+			dec_num = dec_num * base + 1;
+		}
+		else
 		{
 			return (0);
 		}
-		dec_num = dec_num + (b[count] - '0') * power(base, (len - count - 1));
 	}
 	return (dec_num);
 }
