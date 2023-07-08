3914f3b8089a69e9c74e294ab83d1d61de64dcbc
Okyere Adubofuor <oksberg89@gmail.com>
2023-07-07 19:48:30 -0700

    Print bit value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/** get_bit - Prints the value of a bit at an index
+  * @n: The number whose bit at index is to be printed
+  * @index: The position of the bit in the binary representation
+  *
+  * Return: The bit value or -1
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+	size_t bytes, bit;
+
+	if (n == 0 || index == 0)
+	{
+		bit = 0;
+		return (bit);
+	}
+
+	bytes = sizeof(unsigned long int);
+	for (i = bytes * 8 -1; i > 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (i == index)
+		{
+			return (bit);
+		}
+	}
+
+	return (-1);
+}
