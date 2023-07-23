6d354b4167b930f4707004a98f9794e6688d0ec0
Okyere Adubofuor <oksberg89@gmail.com>
2023-07-23 08:20:04 -0700

    I'm a Big guy

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * get_endianness - Checks the endianness
+  *
+  * Return: 1 0r 0
+  */
+
+int get_endianness(void)
+{
+	int num = 0;
+	char *ptr = NULL;
+
+	ptr = (char *)&num;
+
+	if (*ptr == 1)
+		return (1);
+	else
+		return (0);
+}
