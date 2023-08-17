bc2e190a4b99dff1fd24367df9fdf0c06f5500df
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-17 09:30:18 -0700

    Correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,7 +13,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
-		printf("Nothing to print");
+		printf("0 (nil)\n");
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
