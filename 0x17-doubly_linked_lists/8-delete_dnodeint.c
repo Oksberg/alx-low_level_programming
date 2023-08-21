81790cac35414b893c3a49e858f9c589a926b38e
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-21 06:40:45 -0700

    Delete in index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	temp = *head;
 	if (index == 0)
 	{
 		*head = (*head)->next;
@@ -35,13 +34,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	if (temp->next != NULL)
-	{
 		temp->prev->next = temp->next;
-		if (temp->next != NULL)
-			temp->next->prev = temp->prev;
-		free(temp);
-		return (1);
-	}
+
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
 
 	return (1);
 }
