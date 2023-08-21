d52229d5d30ea4ed1ffff4e690186b5a99436dfc
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-21 10:19:58 -0700

    Dynamic library

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,6 @@
+Task 0.
+-------
+In this task, we create a dynamic library call libdynamic.so containing various functions.
+Task 1.
+-------
+In this task, we create a script that creates a dynamic library called liball.so that from all .c files in the current directory.
---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+char *_strcat(char *dest, char *src);
+int _isdigit(int c);
+char *_strncat(char *dest, char *src, int n);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+int _abs(int x);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _atoi(char *s);
+
+#endif
