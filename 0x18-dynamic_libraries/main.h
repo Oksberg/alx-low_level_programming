f7fbf1a9f1dfee652a31260f36f8bfe2aa78e3a0
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-21 18:56:19 -0700

    Call C funtions in Python

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -20,5 +20,10 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _atoi(char *s);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif
