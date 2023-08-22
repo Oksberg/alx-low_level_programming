1effbff41b965a1678679d906e6084559fdfd401
Okyere Adubofuor <oksberg89@gmail.com>
2023-08-21 20:29:30 -0700

    Dynamic library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -15,6 +15,7 @@ int _abs(int x);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
