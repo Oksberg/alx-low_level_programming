7a48f2d23674996194e52824306a1c9887284746
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-08 08:27:17 -0700

    Returns index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -15,7 +15,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index;
 
 	index = hash_djb2(key);
-	
+
 	index = index % size;
 
 	return (index);
