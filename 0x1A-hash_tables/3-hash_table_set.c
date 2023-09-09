d984ecc2637d6feabbb44f2520d491da518e79a6
Okyere Adubofuor <oksberg89@gmail.com>
2023-09-08 18:27:26 -0700

    Inserting a new element

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hast_table_set - Inserts an element to the hash table
+ * @ht: The hash table
+ * @key: The key of the element to add
+ * @value: The value the key inserts
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index ;
+	hash_node_t *current;
+	hash_node_t *new_element;
+
+	if (ht == NULL || value == NULL)
+		return (0);
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	current = ht->array[index];
+	new_element = malloc(sizeof(hash_node_t));
+	if (new_element == NULL)
+		return (0);
+
+	new_element->key = strdup(key);
+	new_element->value = strdup(value);
+	if (new_element->value == NULL)
+	{
+		free(new_element->key);
+		free(new_element);
+		return (0);
+	}
+
+	if (current == NULL)
+		ht->array[index] =  new_element;
+
+	else
+	{
+		while(current->next != NULL)
+			current = current->next;
+
+		current->next = new_element;
+		if (strcmp(current->key, key) == 0)
+		{
+			new_element->next = current;
+			current = current->next;
+		}
+	}
+	return (1);
+}
