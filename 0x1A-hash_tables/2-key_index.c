d31bceb7b89b39db7aa2dd4ab2235fbb130bee97
Okyere <oksberg89@gmail.com>
2023-11-03 17:55:50 -0700

    Keep hashing

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: The key to evaluate
+ * @size: Maximum size of the hash table
+ *
+ * Return: Unsigned long int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+
+	index = index % size;
+
+	return (index);
+}
